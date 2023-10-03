86b446d9e83ddaeb605a52e3be33009d9e2b9771
Emirhan TALA <emirhantala99ff99@gmail.com>
2023-10-03 17:43:07 +0200

    init - blank skill

---
+++ src/main/kotlin/furhatos/app/ethnifurhat/main.kt
@@ -0,0 +1,15 @@
+package furhatos.app.ethnifurhat
+
+import furhatos.app.ethnifurhat.flow.Init
+import furhatos.flow.kotlin.Flow
+import furhatos.skills.Skill
+
+class EthnifurhatSkill : Skill() {
+    override fun start() {
+        Flow().run(Init)
+    }
+}
+
+fun main(args: Array<String>) {
+    Skill.main(args)
+}
