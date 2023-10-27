7416ca9374a2272d064063bcaca2b75e51455d72
berk <83782083+berk0vicc@users.noreply.github.com>
2023-10-27 10:35:59 +0200

    Face changing improved during talking

--- src/main/kotlin/furhatos/app/ethnifurhat/flow/main/Teaching.kt
+++ src/main/kotlin/furhatos/app/ethnifurhat/flow/main/Teaching.kt
@@ -1,34 +1,20 @@
 package furhatos.app.ethnifurhat.flow.main
-import furhat.libraries.standard.GesturesLib
-import furhatos.app.ethnifurhat.setting.nativeLang
 
 import furhatos.app.ethnifurhat.flow.Parent
+import furhatos.app.ethnifurhat.flow.teachingscript.talkingscript
 import furhatos.flow.kotlin.*
 
-import furhatos.gestures.Gestures
-import furhatos.nlu.common.No
-import furhatos.nlu.common.Yes
+
 
 val Teaching: State = state(Parent) {
-    onTime(repeat = 5000) { // every 5 seconds
-        if (users.count > 1) {
-            // Attend to a random user
-            furhat.attend(users.random)
-            delay(1500) // wait for a while before shifting attention
-        }
-    }
 
     onEntry {
-        furhat.say {
-            +"Ladies and Gentlemen."
-            + GesturesLib.ExpressInterest1()
-            +"Today, we delve into a crucial chapter of the Russo-Turkish War of 1877-1878 - the Siege of Plevna, a testament to human courage and determination against overwhelming odds."
-            + "In the annals of military history, the Siege of Plevna stands tall as a pivotal event during the Russo-Turkish War. At it's helm was the indomitable Ottoman commander, Osman Pasha, or Gazi Osman Pasha as he was known, whose unwavering leadership became synonymous with bravery."
-            + "The backdrop of this tale is the Russo-Turkish War of 1877-1878, a conflict between the mighty Russian Empire and the Ottoman Empire. Amidst this turmoil, the Siege of Plevna unfolded between July and December 1877, capturing the world's attention."
-            + "Picture this: Osman Pasha, a figure of unwavering resolve, leading the Ottoman forces against a formidable Russian and Romanian alliance. Despite overwhelming numerical odds, Osman Pasha and his troops displayed exceptional bravery, showcasing the epitome of military courage."
-            + "The siege, a true test of mettle, concluded in December 1877. Despite the remarkable resistance led by Osman Pasha, the Ottoman forces were eventually overcome by the superior might of the Russian and Romanian troops. This marked a significant victory for the Russian Empire and had profound implications for the Ottoman Empire."
-            + "Yet, in the face of defeat, Osman Pasha emerged as a symbol of unwavering resistance. His bravery and tenacity echoed through the corridors of history, immortalizing him as a hero in the Ottoman Empire. His legacy became a beacon of inspiration for generations to come."
-            + "In closing, the Siege of Plevna stands as a testament to the courage and determination of the Ottoman forces during a challenging period in their history. It is a reminder that the human spirit can triumph even in the face of insurmountable odds. As we explore this chapter, let us reflect on the resilience of Osman Pasha and his troops, underscoring the enduring spirit of human bravery."
+        furhat.attend(users.random)
+        for (element in talkingscript) {
+            delay(500)
+            furhat.attend(users.other)
+            furhat.say(element)
+
         }
         goto(Thanks)
     }
---
+++ src/main/kotlin/furhatos/app/ethnifurhat/flow/teachingscript/scripts.kt
@@ -0,0 +1,18 @@
+package furhatos.app.ethnifurhat.flow.teachingscript
+
+val talkingscript = listOf("Ladies and Gentlemen.\n",
+    "Today, we delve into a crucial chapter of the Russo-Turkish War of 1877-1878 - the Siege of Plevna, a testament to human courage and determination against overwhelming odds.",
+    "In the annals of military history, the Siege of Plevna stands tall as a pivotal event during the Russo-Turkish War. ",
+    "At it's helm was the indomitable Ottoman commander, Osman Pasha, or Gazi Osman Pasha as he was known, whose unwavering leadership became synonymous with bravery.",
+    "The backdrop of this tale is the Russo-Turkish War of 1877-1878, a conflict between the mighty Russian Empire and the Ottoman Empire.",
+    "Amidst this turmoil, the Siege of Plevna unfolded between July and December 1877, capturing the world's attention.",
+    "Picture this: Osman Pasha, a figure of unwavering resolve, leading the Ottoman forces against a formidable Russian and Romanian alliance. Despite overwhelming numerical odds, Osman Pasha and his troops displayed exceptional bravery, showcasing the epitome of military courage.",
+    "The siege, a true test of mettle, concluded in December 1877. Despite the remarkable resistance led by Osman Pasha, the Ottoman forces were eventually overcome by the superior might of the Russian and Romanian troops. ",
+    "This marked a significant victory for the Russian Empire and had profound implications for the Ottoman Empire.",
+    "Yet, in the face of defeat, Osman Pasha emerged as a symbol of unwavering resistance. ",
+    " His bravery and tenacity echoed through the corridors of history, immortalizing him as a hero in the Ottoman Empire",
+    "His legacy became a beacon of inspiration for generations to come.",
+    "In closing, the Siege of Plevna stands as a testament to the courage and determination of the Ottoman forces during a challenging period in their history.",
+    "It is a reminder that the human spirit can triumph even in the face of insurmountable odds.",
+    "As we explore this chapter, let us reflect on the resilience of Osman Pasha and his troops, underscoring the enduring spirit of human bravery."
+    )
