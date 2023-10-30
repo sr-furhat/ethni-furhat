3bdb6016eb117e4a8c5fde6feff4e5fab8d40add
Emirhan TALA <emirhantala99ff99@gmail.com>
2023-10-30 21:45:32 +0100

    script update

--- src/main/kotlin/furhatos/app/ethnifurhat/flow/main/Teaching.kt
+++ src/main/kotlin/furhatos/app/ethnifurhat/flow/main/Teaching.kt
@@ -4,21 +4,32 @@ import furhat.libraries.standard.GesturesLib
 import furhatos.app.ethnifurhat.flow.noParent
 import furhatos.flow.kotlin.*
 
-val teachingScript = listOf("Ladies and Gentlemen.\n",
-    "Today, we delve into a crucial chapter of the Russo-Turkish War of 1877-1878 - the Siege of Plevna, a testament to human courage and determination against overwhelming odds.",
-    "In the annals of military history, the Siege of Plevna stands tall as a pivotal event during the Russo-Turkish War. ",
-    "At it's helm was the indomitable Ottoman commander, Osman Pasha, or Gazi Osman Pasha as he was known, whose unwavering leadership became synonymous with bravery.",
-    "The backdrop of this tale is the Russo-Turkish War of 1877-1878, a conflict between the mighty Russian Empire and the Ottoman Empire.",
-    "Amidst this turmoil, the Siege of Plevna unfolded between July and December 1877, capturing the world's attention.",
-    "Picture this: Osman Pasha, a figure of unwavering resolve, leading the Ottoman forces against a formidable Russian and Romanian alliance. Despite overwhelming numerical odds, Osman Pasha and his troops displayed exceptional bravery, showcasing the epitome of military courage.",
-    "The siege, a true test of mettle, concluded in December 1877. Despite the remarkable resistance led by Osman Pasha, the Ottoman forces were eventually overcome by the superior might of the Russian and Romanian troops. ",
-    "This marked a significant victory for the Russian Empire and had profound implications for the Ottoman Empire.",
-    "Yet, in the face of defeat, Osman Pasha emerged as a symbol of unwavering resistance. ",
-    " His bravery and tenacity echoed through the corridors of history, immortalizing him as a hero in the Ottoman Empire",
-    "His legacy became a beacon of inspiration for generations to come.",
-    "In closing, the Siege of Plevna stands as a testament to the courage and determination of the Ottoman forces during a challenging period in their history.",
-    "It is a reminder that the human spirit can triumph even in the face of insurmountable odds.",
-    "As we explore this chapter, let us reflect on the resilience of Osman Pasha and his troops, underscoring the enduring spirit of human bravery."
+val teachingScript = listOf(
+    "Ladies and Gentlemen.",
+    "Today, we'll learn about a brave and skilled leader from the Ottoman Empire named Gazi Osman Pasha.",
+    "He became very famous because of a battle called the \"Battle of Plevna\". Let's dive into his story!",
+    "Gazi Osman Pasha was a military leader or \"pasha\" from the Ottoman Empire.",
+    "He was born in 1832. As he grew up, he showed great skill in military strategy and leadership. ",
+    "In the late 1800s, the Ottoman Empire and the Russian Empire had disagreements. ",
+    "This led to a big war called the Russo-Turkish War in 1877-1878. ",
+    "Many battles happened during this war, but the most famous one is the Battle of Plevna. ",
+    "In 1877, Russian troops wanted to capture a town called Plevna in modern-day Bulgaria. ",
+    "The reason? It was a key place to control if they wanted to move towards the Ottoman capital, Istanbul. ",
+    "But, Gazi Osman Pasha and his soldiers were there to defend it. ",
+    "They built strong defenses around the town and prepared for the Russian attack. ",
+    "The Russians tried many times to take Plevna, but Osman Pasha's strategies and the brave Ottoman soldiers stopped them every time. ",
+    "The battle went on for months! After a long and hard fight, in December 1877, the Ottomans ran out of supplies. ",
+    "Gazi Osman Pasha had to make a tough choice. Instead of surrendering in a usual way, he handed over his sword to the Russian leader as a sign of respect. ",
+    "This action showed his bravery and honor. ",
+    "Even though the Ottomans lost Plevna, Osman Pasha became a hero. ",
+    "The title \"Gazi\" was given to him, which means \"warrior\" or \"veteran.\"",
+    "The story of Gazi Osman Pasha reminds us of the bravery and skills of Ottoman military leaders. ",
+    "Even in difficult times, they showed honor and determination. ",
+    "Osman Pasha's tactics in Plevna are studied in military schools even today.",
+    "Certainly! Gazi Osman Pasha is a symbol of bravery and honor in the history of the Ottoman Empire.",
+    "His leadership at Plevna showed the world the spirit and determination of the Ottoman soldiers.",
+    "The next time you hear about the Russo-Turkish War, remember the hero of Plevna, Gazi Osman Pasha!",
+    "And if you ever come across a war song called the \"Plevna Anthem,\" know that it was composed in honor of this very hero and his valiant stand."
 )
 
 val noTeaching: State = state(noParent) {
